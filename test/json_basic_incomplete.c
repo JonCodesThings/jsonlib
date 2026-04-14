0ca6d143920035602dacc1ca61dee1dc9c991bed
JonCodesThings <jon@joncodesthings.net>
2026-04-14 16:50:04 +0100

    Added basic_incomplete test

---
+++ test/json_basic_incomplete.c
@@ -0,0 +1,33 @@
+#define JSONLIB_IMPLEMENTATION
+#include <include/jsonlib/json.h>
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "json_test_allocator.h"
+
+int main()
+{
+	const char *str = "{";
+
+	InitTESTAllocatorContext();
+	JSONLIB_SetAllocator(TESTAllocate, TESTDeallocate);
+
+	JSON *json = JSONLIB_ParseJSON(str, (u32)strlen(str));
+	
+	assert(json == NULL);
+
+	const char* jsonStr = JSONLIB_MakeJSON(json, 0);
+
+	assert(jsonStr != NULL);
+	assert(!strcmp(str, jsonStr));
+
+	JSONLIB_ClearJSON((void*)jsonStr);
+
+	JSONLIB_FreeJSON(json);
+
+	assert(allocations == 0);
+
+	return 0;
+}
