03bb30ee7c34987e42be3024b5d5d41af9d9220f
JonCodesThings <jon@joncodesthings.net>
2026-04-21 14:26:01 +0100

    Added incomplete array test

---
+++ test/json_basic_array_incomplete.c
@@ -0,0 +1,23 @@
+#define JSONLIB_IMPLEMENTATION
+#include <include/jsonlib/json.h>
+
+#include <assert.h>
+#include <string.h>
+
+#include "json_test_allocator.h"
+
+int main()
+{
+	const char *str = "[";
+
+	InitTESTAllocatorContext();
+	JSONLIB_SetAllocator(TESTAllocate, TESTDeallocate);
+
+	JSON *json = JSONLIB_ParseJSON(str, (u32)strlen(str));
+	
+	assert(json == NULL);
+
+	assert(allocations == 0);
+
+	return 0;
+}
